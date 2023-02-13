d4216fc88b1a8a7df48a0096fa2fa81a123b1fa1
madirapoli20 <madirapoli@gmail.com>
2023-02-13 15:42:57 +0530

    reddy

--- src/main/java/com/slokam/SpringbootHibernate/PatientRepo.java
+++ src/main/java/com/slokam/SpringbootHibernate/PatientRepo.java
@@ -4,5 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 @Repository
 public interface PatientRepo extends JpaRepository<Patient,Integer> {
-
+// hlooooo
+	//bolooo
+	
+	//looo
+	//pooS
 }
