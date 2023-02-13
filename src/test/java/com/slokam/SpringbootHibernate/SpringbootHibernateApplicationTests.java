c08322b92c0f7dc109556b0e99dcd738c213bc68
madirapoli20 <madirapoli@gmail.com>
2023-02-13 15:25:12 +0530

    polibro

--- src/test/java/com/slokam/SpringbootHibernate/SpringbootHibernateApplicationTests.java
+++ src/test/java/com/slokam/SpringbootHibernate/SpringbootHibernateApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringbootHibernateApplicationTests {
-
+  // here i am changing
 	@Test
 	void contextLoads() {
 	}
